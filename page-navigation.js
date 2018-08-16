4364b23e0e291be48369fff2777252e1afd65030
abdallahapps2017 <30426688+abdallahapps2017@users.noreply.github.com>
2018-08-16 04:23:00 -0700

    Update page-navigation.js

--- page-navigation.js
+++ page-navigation.js
@@ -1,6 +1,6 @@
   /*<![CDATA[*/
-    if (typeof firstText == "undefined") firstText = "السابق"; 
-    if (typeof lastText == "undefined") lastText = "التالي";
+    if (typeof firstText == "undefined") firstText = "« السابق"; 
+    if (typeof lastText == "undefined") lastText = "التالي »";
     var noPage;
     var currentPage;
     var currentPageNo;
